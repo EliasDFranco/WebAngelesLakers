d920d1735388055cfbb35117b3f33939bc77f31d
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-25 17:55:34 -0300

    Haciendo las views de equipo y jugadores de Lakers

--- Lakers_web/lakers/views.py
+++ Lakers_web/lakers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Equipo, Jugador
 
-# Create your views here.
+
+
+def equipo_detalle(request):
+    equipo = get_list_or_404(Equipo, nombre="Los Ángeles Lakers")
+    jugadores = Equipo.jugadores.all()
+    return render(request, 'equipo_detalle.html', {'equipo': equipo, 'jugadores': jugadores})
+
+def jugador_detalle(request, jugador_id):
+    jugador = get_list_or_404(Jugador, jugador_id=id)
+    return render(request, 'jugador:detalle.html', { 'jugador: jugador'})
+    
