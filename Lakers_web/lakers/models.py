a2158132e058987473cd54271f5b1f83d40b1fe0
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-25 17:37:26 -0300

    Modelos Equipo y Jugador | Haciendo migración

--- Lakers_web/lakers/models.py
+++ Lakers_web/lakers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Equipo
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=150, default="Los Ángeles Lakers")
+    fundación = models.IntegerField()
+    ciudad = models.CharField(max_length=150, default="Los Ángeles, California")
+    campeonatos = models.IntegerField(default=15)
+    estadio = models.CharField(max_length=200, default="Crypto.com Arena")
+    logo_lakers = models.ImageField(upload_to="equipos/", blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.nombre}"
+    
+# Modelo de Jugador
+class Jugador(models.Model):
+    equipo = models.ForeignKey(Equipo,on_delete=models.CASCADE, related_name="jugador")
+    nombre = models.CharField(max_length=150)
+    posición = models.CharField(max_length=100, default="")
+    número_remera = models.IntegerField()
+    foto_jugador = models.ImageField(upload_to="jugador/", blank=True, null=True )
+    
+    def __str__(self):
+        return f"{self.nombre} - {self.equipo.nombre}"
