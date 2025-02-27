fce06a0b0afcd1bb6ebad46a4efa907609af8339
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-27 10:12:44 -0300

    Agregando la ruta a index

--- Lakers_web/lakers/urls.py
+++ Lakers_web/lakers/urls.py
@@ -4,7 +4,8 @@ from . import views
 #Aqui voy a ir poniendo las urls que va a ir teniendo la app
 
 urlpatterns = [
-    path('equipo/', views.equipo_detalle, name='equipodetalle'),
-    path('jugador/<int:jugador_id>/', views.jugador_detalle, name='jugadordetalle'),
+    path('', views.index, name="index"),
+    path('equipo/', views.equipoDetalle, name='equipoDetalle'),
+    path('jugador/<int:jugador_id>/', views.jugadorDetalle, name='jugadorDetalle'),
 ]
 
