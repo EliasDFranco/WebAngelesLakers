74afe01ce5d5eba528519001d8eb971873759443
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-27 10:13:20 -0300

    Agregando index

--- Lakers_web/lakers/views.py
+++ Lakers_web/lakers/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_list_or_404
 from .models import Equipo, Jugador
 
-def equipo_detalle(request):
+def index(request):
+    return render(request, 'index.html' )
+
+def equipoDetalle(request):
     equipo = get_list_or_404(Equipo, nombre="Los Ángeles Lakers")
     jugadores = equipo[0].jugadores.all()
     return render(request, 'equipoDetalle.html', {'equipo': equipo[0], 'jugadores': jugadores})
 
-def jugador_detalle(request, jugador_id):
+def jugadorDetalle(request, jugador_id):
     jugador = get_list_or_404(Jugador, jugador_id=id)
     return render(request, 'jugadorDetalle.html', { 'jugador': jugador})
     
